94d75e9e68adaa4b3e6ac23c517548ceb5c17941
Kevin Martello <kevinmartello@icloud.com>
2019-08-26 13:13:17 -0500

    String
    Pasar de Mayusculas a  minusculas, saber cuantas letras hay, como acceder a una letra especifica

---
+++ Clase2/clase2.js
@@ -0,0 +1,12 @@
+var nombre = 'kevin';
+var apellido = 'MAYORGA';
+var mayusculas = nombre.toUpperCase();
+var minusculas = apellido.toLowerCase();
+var primeraletranombre = nombre.charAt(0);
+var cantidadletrasdelnombre = nombre.length;
+
+var nombrecompleto = nombre + ' ' + apellido
+var nombrecompleto2 = `${nombre} ${apellido}`
+
+var str = nombre.charAt(1) + nombre.charAt(2);
+var str2 = nombre.substr(1 , 2);
