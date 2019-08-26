6830369b951bf22db206d2a7b095773d451f9f56
Kevin Martello <kevinmartello@icloud.com>
2019-08-26 13:52:45 -0500

    Funciones
    Aprender a definir nuestras propias funciones, pasar parametros cuando las invocamos

---
+++ Clase4/clase4.js
@@ -0,0 +1,11 @@
+var nombre = 'kevin'
+var edad = '25'
+
+function imprimiredad(n, e) {
+  console.log (`${n} tiene ${e} años `)
+}
+
+imprimiredad(nombre , edad)
+imprimiredad('diego' , 24)
+imprimiredad('karen' , 23)
+imprimiredad('anibal' , 30)
