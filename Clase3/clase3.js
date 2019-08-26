ffaea1728c435a3e1332512be61180abe28da5f7
Kevin Martello <kevinmartello@icloud.com>
2019-08-26 13:33:53 -0500

    Números
    Operaciones Comunes, areglar numeros decimales

---
+++ Clase3/clase3.js
@@ -0,0 +1,27 @@
+var edad= 25
+//edad = edad + 1 es lo mismo que:
+edad += 1
+
+var peso = 90
+//peso = peso - 10 es lo mismo que:
+peso -= 10
+
+var hambuerguesa = 1
+
+//peso = peso + hambuerguesa es lo mismo que:
+peso += hambuerguesa
+
+var jugar = 3
+
+//peso = peso - jugar es lo mismo que:
+peso -= jugar
+
+var preciovino = 200.3
+
+var total = Math.round(preciovino *100 * 3 ) / 100
+var totalstr = total.toFixed(2)
+var total2 = parseFloat(totalstr)
+
+var pizza = 8
+var persona = 2
+var cantidaddeprociones = pizza / persona
