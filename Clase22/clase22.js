a3a80397bd91e554231f290c3d88b3c1e10aaeea
Kevin Martello <kevinmartello@icloud.com>
2019-08-30 12:24:58 -0500

    Clases de Js

---
+++ Clase22/clase22.js
@@ -0,0 +1,20 @@
+var kevin = new Persona('Kevin','Mayorga',1.65)
+var karen = new Persona('Kevin', 'Marquez',1.60)
+var jeison = new Persona('Jeison','Vargas',1.82)
+
+function Persona(nombre , apellido, estatura) {
+  this.nombre = nombre
+  this.apellido = apellido
+  this.estatura =estatura
+}
+Persona.prototype.saludar = function () {
+
+    console.log(`hola me llamo: ${this.nombre} ${this.apellido} `)
+}
+
+Persona.prototype.soyalto = function () {
+  return this.estatura > 1.60
+}
+kevin.saludar()
+karen.saludar()
+jeison.saludar()
