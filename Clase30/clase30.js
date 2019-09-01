835420a2fb3d11b8e6dbd8ecb6f3bc36ced73f56
Kevin Martello <kevinmartello@icloud.com>
2019-08-31 19:36:27 -0500

    Manejando el Orden y el Asincronismo en JS

---
+++ Clase30/clase30.js
@@ -0,0 +1,26 @@
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+
+
+
+const Opts = {crossDomain: true}
+
+function obtenerPersonaje(id , callback) {
+  const url= `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+
+  $.get( url, Opts , function (persona) {
+  console.log(`Hola, Yo soy ${persona.name}`)
+
+  if (callback) {
+    callback()
+  }
+
+})
+
+}
+
+obtenerPersonaje(1, function () {
+  obtenerPersonaje(2,function () {
+    obtenerPersonaje(3)
+  })
+})
