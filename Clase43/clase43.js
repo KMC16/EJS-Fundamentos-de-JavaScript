9e3e27bd6330b04defa743afd189b884f0c109e8
Kevin Martello <kevinmartello@icloud.com>
2019-09-03 12:36:02 -0500

    Cambiando de contexto al llamar a una función

---
+++ Clase43/clase43.js
@@ -0,0 +1,22 @@
+// el this no es quien nosotros esperamos que sea
+// el bind cambia el contexto del this
+const kevin ={
+  nombre:'Kevin Martello',
+  apellido:'Mayorga Cleveland'
+}
+
+const karen ={
+  nombre:'Karen ',
+  apellido:'Marquez Lopez'
+
+}
+
+function saludar(saludo='Hola') {
+  console.log(`${saludo} mi nombre es ${this.nombre}`);
+}
+// const saludarKevin= saludar.bind(kevin)
+// const saludarKaren= saludar.bind(karen)
+// setTimeout(saludar.bind(kevin,'hola quiuvo'),1000)
+
+// saludar.call(kevin,'hola quiuvo')
+saludar.apply(kevin, ['Hola,Quiuvo'])
