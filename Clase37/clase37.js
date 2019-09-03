cf644376452da1e301910abac97135495e601f70
Kevin Martello <kevinmartello@icloud.com>
2019-09-03 10:32:23 -0500

    var, let y const: Diferencia entre ellos
    Diferencias entre ellos

---
+++ Clase37/clase37.js
@@ -0,0 +1,26 @@
+var kevin = {
+  nombre:'Kevin',
+  apellido:'Mayorga',
+  edad: 25
+}
+const numeros=[1,2,3,4,5]
+function esMAyorDeEdad(persona) {
+  let mensaje
+  if (persona.edad >= 18) {
+
+    mensaje = 'Es Mayor de edad'
+
+  }
+  else {
+
+      mensaje = 'Es menor de edad'
+  }
+  console.log(mensaje);
+}
+
+// esMAyorDeEdad(kevin)
+
+for (var i = 0; i < 10; i++) {
+console.log(i);
+}
+console.log(`Termino el for, el valor de i es ${i}`);
